8e119f9268109384c991690f756e4954234d5bc2
Evyn Vega <61162398+McNoswald@users.noreply.github.com>
2021-04-29 22:30:50 -0700

    W.I.P Ver 1.2 Code
    Still pretty poorly written because me a nooba.

---
+++ Code/Shell/FS/format.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SiaOS.FS
+{
+    class frm
+    {
+        public static void format()
+        {
+            Console.WriteLine("ARE YOU SURE? ALL DATA WILL BE LOST ON YOUR HARDDRIVE! \n(TYPE YES TO CONFIRM)");
+            if (Console.ReadLine() == "yes")
+            {
+                try
+                {
+                    Console.WriteLine("Formating...");
+                    Kernel.fs.Format(@"0:\", "FAT32", true);
+                    Console.WriteLine("Done!");
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine("A Error Ocurred while trying to format the drive.");
+                    Console.WriteLine(Ex.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Operation Cancelled, No changes were made.");
+            }
+        }
+    }
+}
